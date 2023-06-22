fa4cb1d5d22f34d6df29d658e6e6e32db1d68f54
Utkarsh4517 <uttkarsh.dav@gmail.com>
2023-06-22 19:54:02 +0530

    added basic  texts

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    double width =  MediaQuery.of(context).size.width;
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
@@ -20,6 +22,37 @@ class HomePage extends StatelessWidget {
                     'assets/svg/color-morph.svg',
                     width: MediaQuery.of(context).size.width,
                   ),
+                  Center(
+                    child: Column(
+                      children: [
+                        SizedBox(height: width * 0.1),
+                        Text(
+                          'Frustration is the key to downfall',
+                          style: GoogleFonts.chivo(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: width * 0.05
+                          ),
+                        ),
+                        Text(
+                          'and Frustrating someone else is the key to success!!',
+                          style: GoogleFonts.chivo(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: width * 0.03
+                          ),
+                        ),
+                        Text(
+                          'SO BECOME A PART OF THIS EPIC COMMUNITY BY CONTRIBUTING TO GALI - AI',
+                          style: GoogleFonts.chivo(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: width * 0.01
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
